b84bbeef9e0c53d5a3e0a8e677c0bcd480be42ae
Lawrence Archer <lawrence.archer@gmail.com>
2018-03-13 07:45:59 +0000

    make basic operations work. decimals still WIP

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll('.button');
+const clear = document.querySelector("#clear");
+clear.addEventListener('click', (e) => {
+  location.reload();
+});
+var operands = [];
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
     var x = button.textContent;//run script from here to append number to display / run operations
 		console.log(x);
 		if (/[0123456789]/.test(x)){
 			display.textContent += x;
-		}
-		else if (x == "+"){
-			var operation = display.textContent +"+";
+		}else if (x == "+"){
+			operands.push(display.textContent);
+      operands.push("+");
 			display.textContent = "";
-			console.log(operation);
-		}
+			console.log(operands);
+		}else if (x == "-"){
+			operands.push(display.textContent);
+      operands.push("-");
+			display.textContent = "";
+			console.log(operands);
+		}else if (x == "*"){
+			operands.push(display.textContent);
+      operands.push("*");
+			display.textContent = "";
+			console.log(operands);
+		}else if (x == "/"){
+			operands.push(display.textContent);
+      operands.push("/");
+			display.textContent = "";
+			console.log(operands);
+		}else if(x == "="){
+      operands.push(display.textContent);
+      display.textContent = operate(operands[1], Number(operands[0]), Number(operands[2]));
+      operands = [];
+    };
   });
 });
 
@@ -40,6 +64,9 @@ function operate(operator, input1, input2){
 			return multiply(input1, input2)
 			break;
 		case "/":
+      if (input2 == 0){
+        return "You are a fucking idiot";
+      }
 			return divide(input1, input2)
 			break;
 	}
