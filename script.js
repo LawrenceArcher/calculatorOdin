f36dce747eb941676c8535e11f2cadc0ce0756d0
Lawrence Archer <lawrence.archer@gmail.com>
2018-03-14 07:43:17 +0000

    remove seperate operator functions

--- script.js
+++ script.js
@@ -40,34 +40,22 @@ buttons.forEach((button) => {
 });
 
 
-function add(input1, input2){
-	return input1 + input2;
-}
-function subtract(input1, input2){
-	return input1-input2;
-}
-function multiply(input1, input2){
-	return input1*input2;
-}
-function divide(input1, input2){
-	return input1/input2;
-}
 function operate(operator, input1, input2){
 	switch (operator){
 		case "+":
-			return add(input1, input2)
+			return input1 + input2;
 			break;
 		case "-":
-			return subtract(input1, input2)
+			return input1-input2;
 			break;
 		case "*":
-			return multiply(input1, input2)
+			return input1*input2;
 			break;
 		case "/":
       if (input2 == 0){
         return "You are a fucking idiot";
       }
-			return divide(input1, input2)
+			return input1/input2;
 			break;
 	}
 }
